41c5f9523ea4ce5d874f959306aa825d19d48488
Endivine1337 <sam.geisler@web.de>
2018-11-06 21:17:53 +0000

    Basic player movement and pipe generation

---
+++ Assets/Scripts/PipeFactory.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeFactory : MonoBehaviour {
+    public GameObject PipeSegment;
+
+    private Queue<GameObject> pipes = new Queue<GameObject>();
+    private float offset;
+
+    public float acceleration = 0.1f;
+    public float startSpeed = 1f;
+       
+
+	// Use this for initialization
+	void Start () {
+        for (var i = 0; i < 10; i++)
+        {
+            var segment = Instantiate(PipeSegment);
+            segment.transform.rotation = Quaternion.Euler(0, 0, 90);
+            segment.transform.Translate(0, 0, i * 4f);
+            segment.transform.localScale = Vector3.one * 5;
+            pipes.Enqueue(segment);
+        }
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dt = -(startSpeed + acceleration * Time.timeSinceLevelLoad) * Time.deltaTime;
+        offset += dt;
+
+        if (offset < -4f)
+        {
+            offset += 4f;
+            var segment = pipes.Dequeue();
+            segment.transform.position = new Vector3(0, 0, pipes.Count * 4f + offset);
+            pipes.Enqueue(segment);
+        }
+
+        var i = 0;
+        foreach (var segment in pipes)
+        {
+            segment.transform.position = new Vector3(0, 0, i * 4f + offset);
+            i++;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerSpawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour {
+
+    public GameObject playerPrefab;
+    public float playerHeight = -0.25f;
+    public float playerDistanceToCam = 2.0f;
+	// Use this for initialization
+	void Start ()
+    {
+        var player = Instantiate(playerPrefab);
+        player.transform.Translate(0, playerDistanceToCam, playerHeight);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
